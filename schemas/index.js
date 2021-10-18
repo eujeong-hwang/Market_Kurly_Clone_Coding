c28348f1172b0d67599909f10d94605ae6d494b9
Andrew <seongchan24@gmail.com>
2021-10-18 15:29:30 +0900

    임성찬 작업 전 몽고db연결, .env생성 초기세팅

---
+++ schemas/index.js
@@ -0,0 +1,19 @@
+onst mongoose = require('mongoose')
+
+const connect = () => {
+    mongoose
+        // .connect(process.env.DB_SERVER_HOST, {
+        .connect(process.env.DB_LOCAL_HOST, {
+            useNewUrlParser: true,
+            ignoreUndefined: true,
+            // user: process.env.DB_USER_ID,
+            // pass: process.env.DB_PASSWORD 
+        })
+        .catch((err) => console.log(err))
+}
+
+mongoose.connection.on('error', (err) => {
+    console.error('몽고디비 연결 에러', err)
+})
+
+module.exports = connect
