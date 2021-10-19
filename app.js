853833d0c2629144312a829c1d2f09d72f02e3f3
Eu Jeong Hwang <59908525+hj2bf@users.noreply.github.com>
2021-10-19 11:01:53 +0900

    eujeong post router done

--- app.js
+++ app.js
@@ -6,13 +6,21 @@ const PORT = process.env.PORT;
 const connect = require("./schemas");
 connect();
 
+// cors 연결, cors를 프론트와 연결하기 위해 오픈한 것
+const cors = require('cors')
+app.use(cors());
+
 const usersRouter = require('./routes/user')
+const postsRouter = require('./routes/post')
+// const cartsRouter = require('./routes/carts')
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(express.static('public'))
 
 app.use("/api", usersRouter);
+app.use("/api", postsRouter);
+// app.use("/api", cartsRouter);
 
 const handleListen = () => {
   console.log(`서버가 요청을 받을 준비가 됐어요😀 http://localhost:3000`);
